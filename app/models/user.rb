47670a81bbfcdf469d9200c74d0e180d913bbf9a
北爪康司 <navigator.lp720@gmail.com>
2024-05-26 21:40:35 +0900

    CarrierWave実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     has_many :reservations
-
-end
+    mount_uploader :avatar, AvatarUploader
+    mount_uploader :profile_image, AvatarUploader
+  end
 
