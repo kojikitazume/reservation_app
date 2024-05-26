1df53793504dca056c884fea5064a0104b753a6b
北爪康司 <navigator.lp720@gmail.com>
2024-05-26 15:01:48 +0900

    ユーザー関連編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_26_044107) do
+ActiveRecord::Schema.define(version: 2024_05_26_053127) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2024_05_26_044107) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "profile_image"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
