ec8d4d9ce20fb1bce5002a9197e4921ba0d9066d
北爪康司 <navigator.lp720@gmail.com>
2024-05-27 13:00:21 +0900

    ログアウトエラーの修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,11 +6,9 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
