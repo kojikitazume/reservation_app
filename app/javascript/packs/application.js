169dd2febe1b3130dc22efaef87ae31590e61959
北爪康司 <navigator.lp720@gmail.com>
2024-06-01 21:09:27 +0900

    予約機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
-//= require_tree .
 
 import Rails from '@rails/ujs';
 import Turbolinks from 'turbolinks';
@@ -31,3 +29,4 @@ document.addEventListener('turbolinks:load', () => {
     });
   }
 });
+
